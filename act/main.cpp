3543dcf2b58ab9ac9abf43fbd41d71a1a7418632
francisco.sanchez <francisco.sanchez@localhost>
2011-04-09 15:18:14 +0000

    Primera version completa en C con reconocimiento de posicion + ocr

--- act/main.cpp
+++ act/main.cpp
@@ -7,9 +7,251 @@
 #include <highgui.h>
 #include <stdio.h>
 #include <ctype.h>
+#include "ml.h"
 #endif
 #include <math.h>
+/****OCR****/
+void findX(IplImage* imgSrc,int* min, int* max){
+	int i;
+	int minFound=0;
+	CvMat data;
+	CvScalar maxVal=cvRealScalar(imgSrc->width * 255);
+	CvScalar val=cvRealScalar(0);
+	//For each col sum, if sum < width*255 then we find the min 
+	//then continue to end to search the max, if sum< width*255 then is new max
+	for (i=0; i< imgSrc->width; i++){
+		cvGetCol(imgSrc, &data, i);
+		val= cvSum(&data);
+		if(val.val[0] < maxVal.val[0]){
+			*max= i;
+			if(!minFound){
+				*min= i;
+				minFound= 1;
+			}
+		}
+	}
+}
 
+void findY(IplImage* imgSrc,int* min, int* max){
+	int i;
+	int minFound=0;
+	CvMat data;
+	CvScalar maxVal=cvRealScalar(imgSrc->width * 255);
+	CvScalar val=cvRealScalar(0);
+	//For each col sum, if sum < width*255 then we find the min 
+	//then continue to end to search the max, if sum< width*255 then is new max
+	for (i=0; i< imgSrc->height; i++){
+		cvGetRow(imgSrc, &data, i);
+		val= cvSum(&data);
+		if(val.val[0] < maxVal.val[0]){
+			*max=i;
+			if(!minFound){
+				*min= i;
+				minFound= 1;
+			}
+		}
+	}
+}
+CvRect findBB(IplImage* imgSrc){
+	CvRect aux;
+	int xmin, xmax, ymin, ymax;
+	xmin=xmax=ymin=ymax=0;
+
+	findX(imgSrc, &xmin, &xmax);
+	findY(imgSrc, &ymin, &ymax);
+	
+	aux=cvRect(xmin, ymin, xmax-xmin, ymax-ymin);
+	
+	return aux;
+	
+}
+
+IplImage preprocessing(IplImage* imgSrc,int new_width, int new_height){
+	IplImage* result;
+	IplImage* scaledResult;
+
+	CvMat data;
+	CvMat dataA;
+	CvRect bb;//bounding box
+	CvRect bba;//boundinb box maintain aspect ratio
+	
+	//Find bounding box
+	bb=findBB(imgSrc);
+	
+	//Get bounding box data and no with aspect ratio, the x and y can be corrupted
+	cvGetSubRect(imgSrc, &data, cvRect(bb.x, bb.y, bb.width, bb.height));
+	//Create image with this data with width and height with aspect ratio 1 
+	//then we get highest size betwen width and height of our bounding box
+	int size=(bb.width>bb.height)?bb.width:bb.height;
+	result=cvCreateImage( cvSize( size, size ), 8, 1 );
+	cvSet(result,CV_RGB(255,255,255),NULL);
+	//Copy de data in center of image
+	int x=(int)floor((float)(size-bb.width)/2.0f);
+	int y=(int)floor((float)(size-bb.height)/2.0f);
+	cvGetSubRect(result, &dataA, cvRect(x,y,bb.width, bb.height));
+	cvCopy(&data, &dataA, NULL);
+	//Scale result
+	scaledResult=cvCreateImage( cvSize( new_width, new_height ), 8, 1 );
+	cvResize(result, scaledResult, CV_INTER_NN);
+	
+	//Return processed data
+	return *scaledResult;
+	
+}
+
+char file_path[] = "../OCRf/";
+
+int train_samples = 50;
+int classes= 10;
+CvMat* trainData;
+CvMat* trainClasses;
+
+int size=40;
+
+const int K=10;
+CvKNearest *knn;
+
+
+void getData()
+{
+	IplImage* src_image;
+	IplImage prs_image;
+	CvMat row,data;
+	char file[255];
+	int i,j;
+	for(i =0; i<classes; i++){
+		for( j = 0; j< train_samples; j++){
+			
+			//Load file
+			if(j<10)
+				sprintf(file,"%s%d/%d0%d.pbm",file_path, i, i , j);
+			else
+				sprintf(file,"%s%d/%d%d.pbm",file_path, i, i , j);
+			src_image = cvLoadImage(file,0);
+			if(!src_image){
+				printf("Error: Cant load image %s\n", file);
+				//exit(-1);
+			}
+			//process file
+			prs_image = preprocessing(src_image, size, size);
+			
+			//Set class label
+			cvGetRow(trainClasses, &row, i*train_samples + j);
+			cvSet(&row, cvRealScalar(i));
+			//Set data 
+			cvGetRow(trainData, &row, i*train_samples + j);
+
+			IplImage* img = cvCreateImage( cvSize( size, size ), IPL_DEPTH_32F, 1 );
+			//convert 8 bits image to 32 float image
+			cvConvertScale(&prs_image, img, 0.0039215, 0);
+
+			cvGetSubRect(img, &data, cvRect(0,0, size,size));
+			
+			CvMat row_header, *row1;
+			//convert data matrix sizexsize to vecor
+			row1 = cvReshape( &data, &row_header, 0, 1 );
+			cvCopy(row1, &row, NULL);
+		}
+	}
+}
+
+void train()
+{
+	knn=new CvKNearest( trainData, trainClasses, 0, false, K );
+}
+
+float classify(IplImage* img, int showResult)
+{
+	IplImage prs_image;
+	CvMat data;
+	CvMat* nearest=cvCreateMat(1,K,CV_32FC1);
+	float result;
+	//process file
+	prs_image = preprocessing(img, size, size);
+	
+	//Set data 
+	IplImage* img32 = cvCreateImage( cvSize( size, size ), IPL_DEPTH_32F, 1 );
+	cvConvertScale(&prs_image, img32, 0.0039215, 0);
+	cvGetSubRect(img32, &data, cvRect(0,0, size,size));
+	CvMat row_header, *row1;
+	row1 = cvReshape( &data, &row_header, 0, 1 );
+
+	result=knn->find_nearest(row1,K,0,0,nearest,0);
+	
+	int accuracy=0;
+	for(int i=0;i<K;i++){
+		if( nearest->data.fl[i] == result)
+                    accuracy++;
+	}
+	float pre=100*((float)accuracy/(float)K);
+	if(showResult==1){
+		printf("|\t%.0f\t| \t%.2f%%  \t| \t%d of %d \t| \n",result,pre,accuracy,K);
+		printf(" ---------------------------------------------------------------\n");
+	}
+
+	return result;
+
+}
+
+void test(){
+	IplImage* src_image;
+	IplImage prs_image;
+	CvMat row,data;
+	char file[255];
+	int i,j;
+	int error=0;
+	int testCount=0;
+	for(i =0; i<classes; i++){
+		for( j = 50; j< 50+train_samples; j++){
+			
+			sprintf(file,"%s%d/%d%d.pbm",file_path, i, i , j);
+			src_image = cvLoadImage(file,0);
+			if(!src_image){
+				printf("Error: Cant load image %s\n", file);
+				//exit(-1);
+			}
+			//process file
+			prs_image = preprocessing(src_image, size, size);
+			float r=classify(&prs_image,0);
+			if((int)r!=i)
+				error++;
+			
+			testCount++;
+		}
+	}
+	float totalerror=100*(float)error/(float)testCount;
+	printf("System Error: %.2f%%\n", totalerror);
+	
+}
+
+void basicOCR()
+{
+
+	//initial
+	sprintf(file_path , "../OCR/");
+	train_samples = 50;
+	classes= 10;
+	size=40;
+
+	trainData = cvCreateMat(train_samples*classes, size*size, CV_32FC1);
+	trainClasses = cvCreateMat(train_samples*classes, 1, CV_32FC1);
+
+	//Get data (get images and process it)
+	getData();
+	
+	//train	
+	train();
+	//Test	
+	test();
+	
+	printf(" ---------------------------------------------------------------\n");
+	printf("|\tClass\t|\tPrecision\t|\tAccuracy\t|\n");
+	printf(" ---------------------------------------------------------------\n");
+
+	
+}
+
+/**end OCR**/
 int estaMarcada(CvMat * marcadog ,  CvRect punto){
   CvScalar media;
   CvMat data;
@@ -78,7 +320,7 @@ void leeExamen(char * nombreExamen){
     int npuntosRes;
     float posRes [50] = {0};
 
-    puntosParaTemplate (marcado, itemH, &npuntosHor, posHor);
+    puntosParaTemplate (marcadog, itemH, &npuntosHor, posHor);
    
     puntosParaTemplate (marcadog, itemP, &npuntosRes, posRes);  
 
@@ -91,8 +333,44 @@ void leeExamen(char * nombreExamen){
       }
     }
     
-    IplImage* screenBuffer;
-    screenBuffer=cvCloneImage(marcadoi);
+    CvMat data[8];
+   
+    float puntoVal = posHor[2]-57;
+    CvRect punto =  cvRect( posHor[2]-57, posHor[3]+4, 30, 32 );
+
+    for (int i=0; i < 8; i++) {  
+      cvGetSubRect(marcadog,&data[i], punto);
+      puntoVal -= 55;
+      punto =  cvRect( puntoVal, posHor[3]+4, 30, 32 );
+      
+    }
+    
+    //OCR
+    float num;
+    char ventana[15];
+    char nres[2];
+    IplImage stub, *dst_img,*dst_imgp;
+    
+    CvFont font;
+    cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 2.0, 2.0, 0, 1, CV_AA);
+    
+ 
+    
+
+    for (int i=7; 0 <= i; i--) {  
+      dst_img = cvGetImage(&data[i], &stub);
+      num =  classify(dst_img, 1);
+      printf(" Resultado OCR D %d : %f \n",i,num);
+      sprintf( ventana, "Digito %d\0", i );
+      sprintf( nres, "%d\0", (int)num );
+      dst_imgp = cvCreateImage( cvSize(200, 200) ,  dst_img ->depth,  dst_img ->nChannels );
+      cvResize(dst_img, dst_imgp);
+      cvPutText(dst_imgp, nres, cvPoint(0, 35), &font, cvScalar(0, 0, 0, 0));
+
+      cvShowImage( ventana, dst_imgp   );
+    }
+
+    
     cvNamedWindow( "Demo", CV_WINDOW_AUTOSIZE );
     cvShowImage( "Demo", marcadoi  );
     cvWaitKey(0);
@@ -102,8 +380,14 @@ void leeExamen(char * nombreExamen){
 
 int main( int argc, char** argv )
 {
+
+ 
+   
+
+    printf ("OCR\n");
+    basicOCR();
     printf( "Reconocimiento de examenes\n");
-    leeExamen("./ex-001.jpg");
+    leeExamen("./ex-002.jpg");
     return 0;
 }
 
